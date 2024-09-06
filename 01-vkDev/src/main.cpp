30d06b03142e87b3db7fcfdc3a4fa2ec1a00a5de
Xin Li (m01117) <m01117@metax-tech.com>
2024-09-06 16:52:02 +0800

    Add swap chain image view create code

--- 01-vkDev/src/main.cpp
+++ 01-vkDev/src/main.cpp
@@ -108,6 +108,7 @@ private:
 		pickPhysicalDevice();
 		createLogicalDevice();
 		creataSwapChain();
+		createImageViews();
 	}
 
 	void mainLoop()
@@ -584,8 +585,44 @@ private:
 		swapChainExtent = extent;
 	}
 
+
+	void createImageViews()
+	{
+		swapChainImageViews.resize(swapChainImages.size());
+
+		for (size_t idx = 0; idx < swapChainImageViews.size(); idx++)
+		{
+			VkImageViewCreateInfo createInfo{};
+			createInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+			createInfo.image = swapChainImages[idx];
+			createInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+			createInfo.format = swapChainImageFormat;
+
+			// swizzle color channel
+			createInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+			createInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+			createInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+			createInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+
+			createInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+			createInfo.subresourceRange.baseMipLevel = 0;
+			createInfo.subresourceRange.levelCount = 1;
+			createInfo.subresourceRange.baseArrayLayer = 0;
+			createInfo.subresourceRange.layerCount = 1;
+
+			// create image view
+			if (vkCreateImageView(device, &createInfo, nullptr, &swapChainImageViews[idx]) != VK_SUCCESS) {
+				throw std::runtime_error("failed to create image views!");
+			}
+		}
+	}
+
 	void cleanup()
 	{
+		for (const auto& imageView : swapChainImageViews)
+		{
+			vkDestroyImageView(device, imageView, nullptr);
+		}
 		vkDestroySwapchainKHR(device, swapChain, nullptr);
 		vkDestroyDevice(device, nullptr);
 
@@ -619,6 +656,8 @@ private:
 	VkFormat swapChainImageFormat;
 	VkExtent2D swapChainExtent;
 	std::vector<VkImage> swapChainImages;
+
+	std::vector<VkImageView> swapChainImageViews;
 };
 
 
