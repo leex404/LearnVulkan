ac6cec65c90411285c3ebc55fa3ef472f95de151
Xin Li (m01117) <m01117@metax-tech.com>
2024-09-09 13:55:18 +0800

    Add framebuffer create code

--- 01-vkDev/src/main.cpp
+++ 01-vkDev/src/main.cpp
@@ -130,6 +130,8 @@ private:
 		createImageViews();
 		createRenderPass();
 		createGraphicsPipeline();
+		createFramebuffers();
+
 	}
 
 	void mainLoop()
@@ -857,8 +859,38 @@ private:
 		return shaderModule;
 	}
 
+
+	void createFramebuffers()
+	{
+		swapChainFramebuffers.resize(swapChainImageViews.size());
+
+		for (size_t idx = 0; idx < swapChainImageViews.size(); idx++)
+		{
+			VkImageView attachments[] = { swapChainImageViews[idx] };
+
+			VkFramebufferCreateInfo framebufferInfo{};
+			framebufferInfo.sType = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO;
+			framebufferInfo.renderPass = renderPass;
+			framebufferInfo.attachmentCount = 1;
+			framebufferInfo.pAttachments = attachments;
+			framebufferInfo.width = swapChainExtent.width;
+			framebufferInfo.height = swapChainExtent.height;
+			framebufferInfo.layers = 1;
+
+			if (vkCreateFramebuffer(device, &framebufferInfo, nullptr, &swapChainFramebuffers[idx]) != VK_SUCCESS)
+			{
+				throw std::runtime_error("failed to create framebuffer!");
+			}
+		}
+	}
+
 	void cleanup()
 	{
+		for (auto framebuffer : swapChainFramebuffers)
+		{
+			vkDestroyFramebuffer(device, framebuffer, nullptr);
+		}
+
 		vkDestroyPipeline(device, graphicsPipeline, nullptr);
 		vkDestroyPipelineLayout(device, pipelineLayout, nullptr);
 		vkDestroyRenderPass(device, renderPass, nullptr);
@@ -907,7 +939,7 @@ private:
 	VkPipelineLayout pipelineLayout;
 	VkPipeline graphicsPipeline;
 
-
+	std::vector<VkFramebuffer> swapChainFramebuffers;
 };
 
 
